714775da1e31be3d2f2bbdcf34df7ad547ced75d
Deathmuster <ivishaal20@gmail.com>
2023-02-25 17:13:19 +0530

    E

--- Assets/SpeciesPopulation.cs
+++ Assets/SpeciesPopulation.cs
@@ -2,33 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;    
+using TMPro;
 
 public class SpeciesPopulation : MonoBehaviour
 {
     public GameObject bisonPrefab;
-    public TextMeshProUGUI bisonPopulationGO;
+    public GameObject deerPrefab;
+    public GameObject wolfPrefab;
+    public GameObject rattlesnakePrefab;
+    public GameObject falconPrefab;
+
+    public TextMeshProUGUI bisonPopulationText;
+    public TextMeshProUGUI deerPopulationText;
+    public TextMeshProUGUI wolfPopulationText;
+    public TextMeshProUGUI rattlesnakePopulationText;
+    public TextMeshProUGUI falconPopulationText;
+
     private int bisonPopulation = 30;
+    private int deerPopulation = 20;
+    private int wolfPopulation = 10;
+    private int rattlesnakePopulation = 5;
+    private int falconPopulation = 15;
+
     public Vector3 spawnArea;
+
+    private List<GameObject> deerList = new List<GameObject>();
+    private List<GameObject> rattlesnakeList = new List<GameObject>();
 
     void Start()
     {
-        for (int i = 0; i < bisonPopulation; i++)
+        SpawnSpecies(bisonPrefab, bisonPopulation);
+        SpawnSpecies(deerPrefab, deerPopulation);
+        SpawnSpecies(wolfPrefab, wolfPopulation);
+        SpawnSpecies(rattlesnakePrefab, rattlesnakePopulation);
+        SpawnSpecies(falconPrefab, falconPopulation);
+
+        RotateSpecies(deerList, new Vector3(-77.733f, 0f, 0f));
+        RotateSpecies(rattlesnakeList, new Vector3(255.271f, 0f, 0f));
+    }
+
+    void Update()
+    {
+        UpdateLabel(bisonPopulationText, bisonPopulation);
+        UpdateLabel(deerPopulationText, deerList.Count);
+        UpdateLabel(wolfPopulationText, wolfPopulation);
+        UpdateLabel(rattlesnakePopulationText, rattlesnakeList.Count);
+        UpdateLabel(falconPopulationText, falconPopulation);
+    }
+
+    void SpawnSpecies(GameObject prefab, int population)
+    {
+        for (int i = 0; i < population; i++)
         {
             Vector3 randomPos = new Vector3(Random.Range(-spawnArea.x, spawnArea.x), 
                                             (float)9,  
                                             Random.Range(-spawnArea.z, spawnArea.z));
-            Instantiate(bisonPrefab, randomPos, Quaternion.identity);
+            GameObject obj = Instantiate(prefab, randomPos, Quaternion.identity);
+            if (prefab == deerPrefab)
+            {
+                deerList.Add(obj);
+            }
+            else if (prefab == rattlesnakePrefab)
+            {
+                rattlesnakeList.Add(obj);
+            }
         }
-
-        UpdateLabel();
     }
 
-    void UpdateLabel()
+    void UpdateLabel(TextMeshProUGUI text, int population)
     {
-        bisonPopulationGO.text = "Bison Population: " + bisonPopulation;
+        text.text = text.name + ": " + population;
+    }
+
+    void RotateSpecies(List<GameObject> speciesList, Vector3 rotation)
+    {
+        foreach (GameObject obj in speciesList)
+        {
+            obj.transform.Rotate(rotation);
+        }
     }
 }
